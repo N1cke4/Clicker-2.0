cd883524edeec606caa248e0865e92be73adcb07
N1cke4 <nikitazirnov15@gmail.com>
2024-05-27 21:10:01 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp6/DelayPUbey.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp6
+{
+    internal class DelayPUbey
+    {
+        private int count = 0;
+        private int time = 1;
+        private Action action;
+        private System.Windows.Forms.Timer timer;
+        public void Start(System.Windows.Forms.Timer timer, int time, Action action)
+        {
+            this.time = time;
+            this.action = action;
+            this.timer = timer;
+        }
+
+        public void Tick()
+        {
+            count++;
+            if (count > time)
+            {
+                action();
+                timer.Stop();
+                count = 0;
+               
+            }
+
+        }
+    }
+}
+
---
+++ WinFormsApp6/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Specialized;
+using System.Windows.Forms;
+
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        DelayPUbey delay = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+     
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            delay.Tick();
+
+        }
+
+
+
+
+
+        private void cokisinzoom()
+        {
+            pictureBox1.Size = new(350, 350);
+            pictureBox1.Location = new(347, 131);
+            pictureBox1.Image = Resource1.cookis2;
+        } 
+        private void cokisoutzoom()
+        {
+            pictureBox1.Size = new(444, 444);
+            pictureBox1.Location = new(303, 83);
+            pictureBox1.Image = Resource1.cookis;
+        }
+
+        int count = 0;
+
+
+        private void showCookis()
+        { 
+            cokisoutzoom();
+        }
+
+
+
+
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+          timer1.Start();
+            cokisinzoom();
+            delay.Start(timer1, 3, showCookis); 
+            count++;
+            label1.Text = "Счет: " + count.ToString() + "¢";
+        }
+    }
+}
